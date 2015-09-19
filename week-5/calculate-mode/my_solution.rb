193f79f3a7a65520c11b97a2915b99941c443cf9
eshou1014 <eshou1014@gmail.com>
2015-09-19 17:46:17 -0400

    Finished refactoring

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -28,6 +28,38 @@
 =end
 
 # 1. Initial Solution
+# def mode(input)
+# h = Hash.new
+# input.each do |element|
+#   if h[element]
+#     h[element] += 1
+#   else
+#     h[element] = 1
+#   end
+# end
+# puts h
+
+# greatest_value = 0
+# mode_array = []
+
+# h.each do |key,value|
+#   if value >= greatest_value
+#     if value != greatest_value
+#       mode_array = []
+#     end
+#     mode_array.push(key)
+#     greatest_value = value
+
+#   end
+# end
+# return mode_array
+# end
+
+
+
+
+
+# 3. Refactored Solution
 def mode(input)
 h = Hash.new
 input.each do |element|
@@ -37,7 +69,6 @@ input.each do |element|
     h[element] = 1
   end
 end
-puts h
 
 greatest_value = 0
 mode_array = []
@@ -56,12 +87,15 @@ return mode_array
 end
 
 
-
-
-
-# 3. Refactored Solution
-
-
-
-
 # 4. Reflection
+=begin
+  Well we were really hesitant about this challenge because at first, I was wondering how we could put the hash into an array. I didn't realize until later that you had to create your own array to push the keys of the hash into.
+
+  I was able to break down the problem into smaller chunks. However, I still need to work on my pseudocode since although most of the logic is right, I still do not know how to translate that into actual code.
+
+  The biggest issue we ran into was how to get started and how to finish. We knew we had to create a new hash and iterate over it but did not know how. Also we did not know how to put the answer into an array and push the key to the array.
+
+  I did not use any methods. The logic was purely based on if statements and iterators. I found some while refactoring and there were some such as select, each_pair, value, shift, but I did not know how to implement them into the code.
+
+
+=end
